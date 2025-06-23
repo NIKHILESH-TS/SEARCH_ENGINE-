64b148ca7110f420265747a4ff061bbc2a481959
NIKHILESH-TS <codingnikhits@gmail.com>
2025-06-23 21:58:56 +0530

    Added Blocked Domains

--- scipts/final_bot.py
+++ scipts/final_bot.py
@@ -25,6 +25,8 @@ def can_crawl(url):
         print(f"[WARNING] Couldn't retrieve robots.txt ({e}). Defaulting to allowed.")
         return True
     
+# --- Blocked Domains ---
+BLOCKED_DOMAINS = {"google.com", "youtube.com", "facebook.com", "twitter.com", "instagram.com", "linkedin.com", "pinterest.com", "tiktok.com", "reddit.com", "snapchat.com", "tumblr.com", "wikipedia.org", "yahoo.com", "bing.com", "baidu.com", "yandex.ru", "vk.com", "qq.com", "weibo.com", "naver.com", "daum.net", "sina.com.cn", "sohu.com", "163.com", "alibaba.com", "jd.com", "taobao.com", "tmall.com", "amazon.com", "ebay.com", "etsy.com", "craigslist.org", "walmart.com", "bestbuy.com", "target.com", "costco.com", "homedepot.com", "lowes.com", "ikea.com", "wayfair.com", "overstock.com", "zillow.com", "trulia.com", "realtor.com", "airbnb.com", "booking.com", "expedia.com", "tripadvisor.com", "hotels.com", "kayak.com", "skyscanner.com", "orbitz.com", "travelocity.com", "priceline.com", "southwest.com", "delta.com", "united.com", "americanairlines.com", "jetblue.com", "alaskaair.com", "virginamerica.com", "spirit.com", "frontierairlines.com", "ryanair.com", "easyjet.com", "lufthansa.com", "britishairways.com", "airfrance.com", "klm.com", "emirates.com", "qatarairways.com","patreon.com", "kickstarter.com", "indiegogo.com", "gofundme.com", "crowdfunder.com", "crowdsupply.com", "seedrs.com", "crowdcube.com", "fundable.com", "startsomegood.com", "fundrazr.com", "change.org", "avaaz.org", "moveon.org", "care2.com", "actionnetwork.org", "sumofus.org", "greenpeace.org", "wwf.org", "amnesty.org", "humanrightsfirst.org", "hrw.org", "icrc.org", "unicef.org", "who.int", "un.org", "worldbank.org", "imf.org", "oecd.org", "europa.eu", "nato.int", "cia.gov", "fbi.gov", "usps.com"}
 
 # --- HTML Link Parser ---
 def parse_links(hyperlinks, base_url):
@@ -41,6 +43,9 @@ def parse_links(hyperlinks, base_url):
         elif not href.startswith("http"):
             continue
 
+        parsed_domain = urlparse(href).netloc.lower()
+        if any(blocked in parsed_domain for blocked in BLOCKED_DOMAINS):
+            continue  # Skip blocked domains
 
         parsed_urls.append(href.split("#")[0])
     return parsed_urls
