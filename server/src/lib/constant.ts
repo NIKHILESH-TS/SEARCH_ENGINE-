6e651353c1366e0fff15e6b2bb544356aa4d67ee
D3LET3-57 <lalithsaichakilam3@gmail.com>
2025-06-24 13:53:59 +0530

    Configuring Algolia

---
+++ server/src/lib/constant.ts
@@ -0,0 +1,3 @@
+export const INSTANT_SEARCH_INDEX_NAME = 'dataset'; // Your dataset index name
+export const INSTANT_SUGGESTION_INDEX = 'dataset_query_suggestion'; // Your suggestion index name
+export const INSTANT_SEARCH_HIERARCHICAL_ATTRIBUTES = ['categories', 'title', 'description']; // Attributes to use for hierarchical search
